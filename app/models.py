dc3a87668dcd3f2202250110a05095ac692f419c
Tom Montfort <tjmontfort12@gmail.com>
2023-02-16 10:30:13 -0500

    added comments

--- app/models.py
+++ app/models.py
@@ -2,10 +2,12 @@ from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
+# Tells flask how to get user object given an id
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
+# User table
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,41 @@
 from flask import redirect, flash, render_template, request
 from app import app
 from app.forms import LoginForm, CreateAccountForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
 
+# Landing page containing list of generated Recipes
+# login_required decorator will redirect user if they are not logged into an account
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
     return "Recipes!"
 
+# Login page
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # If user is already logged in, redirect them to landing page
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = LoginForm()
+
+    # Ensures that all form fields were validated and had no errors
     if form.validate_on_submit():
-        user = User.query.filter_by(usernam=form.username.data).first()
+
+        # Will query user based on username submitted in form
+        user = User.query.filter_by(username=form.username.data).first()
+
+        # If a user with the username dne or the user entered the wrong password
+        # display error to user and redirect back to login page. 
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user)
         next_page = request.args.get('next')
+
+        # If next_page url param dne or is not valid, will redirect to landing page
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
@@ -30,17 +43,22 @@ def login():
     # return render_template('login.html', title='Log In', form=form)
     return 'Login page'
 
+# Logout page
 @app.route('/logout')
 def logout():
     logout_user()
     return redierct(url_for('index'))
 
+# Create account page
 @app.route('/create-account', methods=['GET', 'POST'])
 def create_account():
+    # If user is already logged in, redirect them to landing page
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = CreateAccountForm()
     if form.validate_on_submit():
+
+        # Create a user, set the password, commit the user to the db and redirect to the login page
         user = User(username=form.username.data, email=form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
