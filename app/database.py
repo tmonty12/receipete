84278df650f85830df99add501c9fe891f45073c
Ben Canfield <becanfie@syr.edu>
2023-02-21 17:52:04 -0500

    Updated Modesl with Food Database

--- app/database.py
+++ app/database.py
@@ -4,14 +4,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 
-osdir = os.path.abspath(os.path.dirname(__file__))
-
-app.config['SQLALCHEMY_DATABASE_URL'] = \
-    'sqlite:///' + os.path.join(osdir, 'pantry.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# app.config['SECRET KEY'] = "guess-me"
-db = SQLAlchemy(app)
+# osdir = os.path.abspath(os.path.dirname(__file__))
+#
+# app.config['SQLALCHEMY_DATABASE_URL'] = \
+#     'sqlite:///' + os.path.join(osdir, 'pantry.db')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#
+# # app.config['SECRET KEY'] = "guess-me"
+# db = SQLAlchemy(app)
 
 
 class Food(db.Model):
