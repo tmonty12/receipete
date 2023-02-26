5324bb42cc693ba24901a1b95ab53f18c23e8fa0
Cayden <ctlombar@syr.edu>
2023-02-25 21:02:52 -0500

    Lombard - 02/25

---
+++ app/tests.py
@@ -0,0 +1,26 @@
+import os
+os.environ['DATABASE_URL'] = 'sqlite://'
+
+from datetime import datetime, timedelta
+import unittest
+from app import app, db
+from app.models import User, Post
+
+class UserModelCase(unittest.TestCase):
+    def setUp(self):
+        self.app_context = app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_password_hashing(self):
+        u = User.query.first()
+        self.assertFalse(u.check_password('dog'))
+        self.assertTrue(u.check_password(u.password))
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
